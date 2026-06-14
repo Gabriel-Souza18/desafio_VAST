e910934133d7a7aac5b99980636891446116450d
Gabriel-Souza18 <gabrielsisou@gmail.com>
2026-06-14 15:48:47 -0300

    Tratando  NaN, colocando Medianas em valores faltantes

--- questao1.py
+++ questao1.py
@@ -2,11 +2,34 @@ import pandas as pd
 import numpy as np
 import matplotlib as plt
 
-def main():
-    df = pd.read_csv("MC1/mc1-reports-data.csv")
-    print(df.head)
-    print(df.info)
+
+
+def infos(df:pd.DataFrame):
+    print("Bairros:")
     print(df['location'].unique())
 
+def preencherBairro(df: pd.DataFrame, coluna):
+    df[coluna]= df.groupby('location')[coluna].transform(
+        lambda x :x.fillna( x.median())
+    )
+    return df
 
-main()
+def tratar_dados(df: pd.DataFrame):
+    df['time']= pd.to_datetime(df['time'])
+
+
+    df_zeros = df.fillna(0)
+    print("Percentual NAN por coluna: \n"+ str(df.isnull().mean()*100))
+    colunas_faltantes = ['sewer_and_water', 'medical', 'buildings', 'shake_intensity']
+    for c in colunas_faltantes:
+        df = preencherBairro(df, c)
+
+    print("Percentual NAN por coluna: \n"+ str(df.isnull().mean()*100))
+
+    print("Media dfZero: "+str(df_zeros.mean()))
+    print("Media dfmedias: "+str(df.mean()))
+    
+
+df = pd.read_csv("MC1/mc1-reports-data.csv")
+infos(df)
+tratar_dados(df)
