8c78bd577be139eeae1e7b435f10fe96c004d635
Gabriel-Souza18 <gabrielsisou@gmail.com>
2026-06-14 15:55:49 -0300

    adiconando comentario sobre tratamento

--- questao1.py
+++ questao1.py
@@ -29,7 +29,9 @@ def tratar_dados(df: pd.DataFrame):
     print("Media dfZero: "+str(df_zeros.mean()))
     print("Media dfmedias: "+str(df.mean()))
     
-
+    'Aqui é melhor colocar a melhor colocar a media do bairro pq fica mais realista do que usar o 0,'
+    'pq se remover vai ter coluna que vamos ter que apagar metade dos dados'
+    
 df = pd.read_csv("MC1/mc1-reports-data.csv")
 infos(df)
 tratar_dados(df)
